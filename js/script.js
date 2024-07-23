751ef63f35c95713c0f44926876eee7b5950ae1c
prince5607 <127681677+prince5607@users.noreply.github.com>
2024-07-23 19:19:42 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+import { addSkill } from "../utils/skillUpdate.js";
+import { addProject } from "../utils/projectsUpdate.js";
+
+document.querySelector('.service-js').innerHTML = addSkill();
+document.querySelector('.projects-container-js').innerHTML = addProject();
+
 const header = document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY>200);
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 200);
 });
 
 let menu = document.querySelector('#menu-icon');
@@ -14,7 +21,7 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     ul.classList.remove('open');
 };
-ScrollReveal().reveal(".name",{distance: '40px',duration:2050,delay: 200,reset:true,origin:'top'});
-ScrollReveal().reveal(".img, .about-text, .servicebox , .contact1, .contact2 ",{distance: '40px',duration:2050,delay: 200,reset:true,origin:'bottom'});
-ScrollReveal().reveal(".about-text h2, .service-text, .contact2 h2",{distance: '40px',duration:2050,delay: 200,reset:true,origin:'top'});
+ScrollReveal().reveal(".name", { distance: '40px', duration: 2050, delay: 200, reset: true, origin: 'top' });
+ScrollReveal().reveal(".img, .about-text, .servicebox,.projectbox , .contact1, .contact2 ", { distance: '40px', duration: 2050, delay: 200, reset: true, origin: 'bottom' });
+ScrollReveal().reveal(".about-text h2, .service-text,.projects-text, .contact2 h2", { distance: '40px', duration: 2050, delay: 200, reset: true, origin: 'top' });
 
