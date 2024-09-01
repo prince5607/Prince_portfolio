d761d53b40de929021b0ae904c7c5daae490a4a2
prince5607 <127681677+prince5607@users.noreply.github.com>
2024-09-01 21:49:46 +0530

    Update projectsUpdate.js

--- utils/projectsUpdate.js
+++ utils/projectsUpdate.js
@@ -2,31 +2,31 @@
     link : 'https://prince5607.github.io/Landing_page/',
     img : 'img/projectImg/landing-page.png',
     name : 'Landing Page',
-    desc : 'used to perform basic calculation'
+   
 },
 {
     link : 'https://github.com/prince5607/Online-Railway-Reservation',
     img : 'img/projectImg/train-rail-logo.jpg',
     name : 'Railway Reseravation System',
-    desc : 'used to perform basic calculation'
+   
 },
 {
     link : 'https://prince5607.github.io/Amazon-Clone-Frontend-/',
     img : 'img/projectImg/amazon.jpg',
     name : 'Amazon clone (Frontend)',
-    desc : 'used to perform basic calculation'
+    
 },
 {
     link : 'https://prince5607.github.io/calculator/',
     img : 'img/projectImg/istockphoto-544462430-1024x1024.jpg',
     name : 'Calculator',
-    desc : 'used to perform basic calculation'
+    
 },
 {
     link : 'https://github.com/prince5607/Banking_System',
     img : 'img/projectImg/internet-banking-vector.jpg',
     name : 'Banking System',
-    desc : 'used to perform basic calculation'
+    
 }
 ]
 
@@ -37,7 +37,6 @@ export function addProject(){
                 <a href="${project.link}" target = "-main">
                 <img src="${project.img}" width="100px" height="100px" style="border-radius: 5%;">
                 <h3>${project.name}</h3>
-                <p>${project.desc}</p>
             </a>
             </div>`;
     });
